13e3ef6c03d70a1a803f7e984c4ecc5ea37d7375
Madhusudan Tripathy <madhusudantripathy77@gmail.com>
2023-01-13 14:52:24 +0530

    Polymorphism Assignment Updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,18 +5,19 @@ public class Main {
     B b = new B();
     System.out.println(b.meth());
   }
-}
+  class A {
 
-class A {
-
-  public String meth(){
-    return "Invoking method from class A";
+    public String meth(){
+      return "Invoking method from class A";
+    }
   }
-}
-class B extends A{
+  class B extends A{
 
-  @Override
-  public String meth() {
-    return "Method is overridden in Extendend class B";
+    @Override
+    public String meth() {
+      return "Method is overridden in Extendend class B";
+    }
   }
+
 }
+
